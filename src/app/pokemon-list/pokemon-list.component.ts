05a238e84342bf5d6595ac72672264bdb4dcb459
netosilveira <jotabtw@gmail.com>
2021-10-28 23:15:51 -0300

    Removing static data from pokemon list

--- src/app/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { Type } from 'src/_model/type';
-import { Pokemon } from 'src/_model/pokemon'
+import { Type } from '../../_model/Type';
+import { Pokemon } from '../../_model/Pokemon';
+import { PokemonService } from 'src/_services/pokemon.service';
 @Component({
   selector: 'app-pokemon-list',
   templateUrl: './pokemon-list.component.html',
@@ -9,98 +10,12 @@ import { Pokemon } from 'src/_model/pokemon'
 
 export class PokemonListComponent {
    
-  public pokemons : Pokemon[] = [{
-    image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png',
-    name: 'Bubasaur',
-    number: 1,
-    types: [
-      Type.Grass,
-      Type.Poison,
-    ]
-  },
-  {
-    image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/002.png',
-    number: 2,
-    name: 'Teste 2',
-    types: [
-      Type.Grass,
-      Type.Poison,
-    ]
-  },
-  {
-    image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/003.png',
-    number: 3,
-    name: 'Teste x',
-    types: [
-      Type.Grass,
-      Type.Poison,
-    ]
-  },
-  {
-    image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/004.png',
-    number: 4,
-    name: 'Teste x',
-    types: [
-      Type.Grass,
-      Type.Poison,
-    ]
-  },
-  {
-    image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/005.png',
-    number: 5,
-    name: 'Teste x',
-    types: [
-      Type.Grass,
-      Type.Poison,
-    ]
-  },
-  {
-    image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/006.png',
-    number: 6,
-    name: 'Yves',
-    types: [
-      Type.Poison,
-      Type.Poison,
-    ]
-  },
-  {
-    image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/007.png',
-    number: 7,
-    name: 'Teste x',
-    types: [
-      Type.Grass,
-      Type.Poison,
-    ]
-  },
-  {
-    image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/091.png',
-    name: 'Bubasaur',
-    number: 8,
-    types: [
-      Type.Grass,
-      Type.Poison,
-    ]
-  },
-  {
-    image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/022.png',
-    number: 9,
-    name: 'Teste 2',
-    types: [
-      Type.Grass,
-      Type.Poison,
-    ]
-  },
-  {
-    image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/020.png',
-    number: 10,
-    name: 'Teste x',
-    types: [
-      Type.Grass,
-      Type.Poison,
-    ]
+  public pokemons : Pokemon[] = [];
+
+  constructor(
+    public pokemonService : PokemonService,
+  ){
+
   }
-
-];
-
 
 }
