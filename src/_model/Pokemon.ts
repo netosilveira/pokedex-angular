219ded65ef51d61d4c41a97032b29405b603fa97
netosilveira <jotabtw@gmail.com>
2021-10-28 23:13:01 -0300

    Pokemon getters

--- src/_model/Pokemon.ts
+++ src/_model/Pokemon.ts
@@ -1,9 +1,31 @@
-import { Type } from "./type";
 
-export interface Pokemon{
-    image: string;
-    number: number;
-    name: string;
-    types: Type[];
+import { Type } from './Type';
+
+export interface Pokemon {
+  image: string;
+  number: number;
+  name: string;
+  types: Type[];
 }
-  
+
+export function getPokemonName(pokemon : Pokemon) : string{
+    return pokemon.name;
+}
+
+export function getPokemonNumber(pokemon: Pokemon): string {
+    return formattingZero(pokemon.number, 3);
+  }
+
+export function getPokemonImage(pokemon: Pokemon): string {
+  return `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${getPokemonNumber(pokemon)}.png`;
+}
+
+export function formattingZero(number: number | string, size: number): string {
+  let string = String(number);
+
+  while (string.length < (size || 2)) {
+    string = '0' + string;
+  }
+
+  return string;
+}
