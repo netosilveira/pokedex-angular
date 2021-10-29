650bbaf78d9a074f9374da7cea6c8c0d7a390fd8
netosilveira <jotabtw@gmail.com>
2021-10-28 23:14:24 -0300

    using pokeApi to get data

--- src/_services/pokemon.service.ts
+++ src/_services/pokemon.service.ts
@@ -1,8 +1,37 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Pokemon } from '../_model/Pokemon';
+import { from } from 'rxjs';
+import { map, mergeMap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PokemonService {
-  constructor() {}
+  public pokemons: Pokemon[] = [];
+
+  constructor(private httpClient: HttpClient) {
+    const urlAPI = 'https://pokeapi.co/api/v2/pokemon/?limit=200';
+
+    this.httpClient
+      .get<any>(urlAPI)
+      .pipe(
+        map((value) => value.results),
+        map((value: any) => {
+          return from(value).pipe(
+            mergeMap((v: any) => this.httpClient.get(v.url))
+          );
+        }),
+        mergeMap((value) => value)
+      )
+      .subscribe(
+        (result: any) =>
+          (this.pokemons[result.id] = {
+            image: result.sprites.front_default,
+            number: result.id,
+            name: result.name,
+            types: result.types.map((t: { type: { name: any; }; }) => t.type.name),
+          })
+      );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
 import { PokemonCardComponent } from './pokemon-card/pokemon-card.component';
+import { PokemonService } from 'src/_services/pokemon.service';
 
 @NgModule({
   declarations: [
@@ -14,9 +16,13 @@ import { PokemonCardComponent } from './pokemon-card/pokemon-card.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    HttpClientModule,
+    PokemonService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
